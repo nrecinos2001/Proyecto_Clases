7344d8177f769495d709e95d9817a2987228055d
00083120@uca.edu.sv <00083120@uca.edu.sv>
2020-08-08 18:59:06 -0600

    Third class

---
+++ PHP/Clase_3/index.php
@@ -0,0 +1,119 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Clase 3</title>
+</head>
+<body>
+    
+    <?php
+    
+    $a = 10;
+    $b = 11;
+    $c = 4;
+    /* echo "<h1>IF, ELSE, IF ELSE</h1>";
+    if ($a > $b) {
+        echo"a es mayor que b";
+    }else if($a == $b){
+        echo"a es igual a b";
+    }else{
+        echo"b es mayor que a";
+    }
+ */
+    /* $name = "Dany";
+    $name2 = "Dany";
+    if($name == "Abner" || $name == "Kelvin"){
+       echo "Hola, $name!";
+    }else{
+        echo"Nel, no te conozco";
+    } */
+
+
+    /* echo "<h1>Operador ternario.</h1>";
+    echo ($a > $b) ? "a es mayor que b" : "b es mayor que a"; */
+
+    /* echo "<h1>Switch.</h1>";
+    switch ($c) {
+        case 1:
+            echo"Este es el numero 1";
+            break;
+        case 2:
+            echo"Este es el numero 2";
+            break;
+        case 3:
+            echo"Este es el numero 3";
+            break;           
+        default:
+            echo"no se encuentra en nuestros parametros";
+            break;
+    } */
+
+    /* $d = 20;    
+    echo"Ejercicio numero par o impar. <br>";
+
+    if(($d % 2) == 0){
+        echo "El numero $d es par (if) <br>";
+    }else{
+        echo "El numero $d es impar (if) <br>";
+    }
+
+    $module = $d % 2;
+
+    switch ($module) {
+        case 0:
+            echo "El numero $d es par (Switch) <br>";
+            break;
+        case 1:
+            echo "El numero $d es impar (Switch) <br>";
+            break;
+
+    } */
+    
+    /* $month = "ene";
+    
+    switch ($month) {
+        case 'ene':
+            echo"El numero de mes es 1";
+        break;
+        case 'feb':
+            echo"El numero de mes es 2";
+        break;
+        case 'mar':
+            echo"El numero de mes es 3";
+        break;
+        case 'abr':
+            echo"El numero de mes es 4";
+        break;
+        case 'may':
+            echo"El numero de mes es 5";
+        break;
+        case 'jun':
+            echo"El numero de mes es 6";
+        break;
+        case 'jul':
+            echo"El numero de mes es 7";
+        break;
+        case 'aug':
+            echo"El numero de mes es 8";
+        break;
+        case 'sep':
+            echo"El numero de mes es 9";
+        break;
+        case 'oct':
+            echo"El numero de mes es 10";
+        break;
+        case 'nov':
+            echo"El numero de mes es 11";
+        break;
+        case 'dic':
+            echo"El numero de mes es 12";
+            break;
+        default:
+            echo"Ese mes no existe";
+            break;
+    } */
+    ?>
+
+</body>
+</html>
